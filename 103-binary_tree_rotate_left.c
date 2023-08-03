3802da50990e57309c848e17d25ca13b92f3eb93
MichaelKazembe <michaelkazembe@gmail.com>
2023-08-03 15:30:47 -0700

    update

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,34 +1,45 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_left - Perform a left rotation on a binary tree
- * @tree: Pointer to the root node of the tree to rotate
- * Return: Pointer to the new root node of the rotated tree
+ * binary_tree_rotate_left - Rotates a node to the left.
+ * @tree: A pointer to the node to rotate.
+ *
+ * Return: The new root node.
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	if (tree == NULL || tree->right == NULL)
-		return (tree);
+	binary_tree_t *original_root;
+	binary_tree_t *new_root;
+	binary_tree_t *new_root_left_child;
 
-	binary_tree_t *new_root = tree->right;
-	tree->right = new_root->left;
+	if (tree == NULL)
+		return (NULL);
 
-	if (new_root->left != NULL)
-		new_root->left->parent = tree;
+	original_root = tree;
+	new_root = original_root->right;
 
-	new_root->left = tree;
-	new_root->parent = tree->parent;
-	tree->parent = new_root;
+	if (new_root != NULL)
+	{
+		new_root->parent = original_root->parent;
+		new_root_left_child = new_root->left;
+		new_root->left = original_root;
+
+		if (original_root->parent != NULL)
+		{
+			if (original_root->parent->left == original_root)
+				original_root->parent->left = new_root;
+			if (original_root->parent->right == original_root)
+				original_root->parent->right = new_root;
+		}
+
+		original_root->right = new_root_left_child;
+		original_root->parent = new_root;
+
+		if (new_root_left_child != NULL)
+		{
+			new_root_left_child->parent = original_root;
+		}
+	}
 
 	return (new_root);
-}
-
-/**
- * rotate_left - Perform a left rotation on a binary tree
- * @tree: Pointer to the root node of the tree to rotate
- * Return: Pointer to the new root node of the rotated tree
- */
-binary_tree_t *rotate_left(binary_tree_t *tree)
-{
-	return (binary_tree_rotate_left(tree));
 }
